c0ca74465df4545542353c7e5e4fd0ea0b55020a
opeyemidy <opeyemidy@gmail.com>
2020-06-11 21:37:22 +0100

    workspace init

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,17 +1,10 @@
 <!-- <div class="pipi fixed-top"></div> -->
 
 <script>
-<<<<<<< HEAD
 export default {}
 </script>
 
 <style lang="scss" scoped>
-=======
-export default {};
-</script>
-
-<style scoped>
->>>>>>> 65f1cd05c7db4cf8b60fe0bf3929b94d96e6da7b
 .pipi {
   position: fixed;
   width: 100%;
